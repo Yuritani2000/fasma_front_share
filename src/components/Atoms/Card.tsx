ee1845d36366c70f19d212f2c0667beef9bab122
Takashi Nishikawa <program.takashi@gmail.com>
2020-10-05 17:15:24 +0900

    [fix] Cardをタグとして利用できるように変更

--- src/components/Atoms/Card.tsx
+++ src/components/Atoms/Card.tsx
@@ -1,31 +1,19 @@
-import React from 'react';
 import styled from 'styled-components';
-
-type Props = {
-    styleProps: StyledCardProps
-}
-
-const Card: React.FC<Props> = (props) => {
-    return (
-        <StyledCard cardSize={props.styleProps.cardSize} radiusSize={props.styleProps.radiusSize} backgroundColor={props.styleProps.backgroundColor} >
-        </StyledCard >
-    )
-}
 
 export default Card;
 
-type StyledCardProps = {
+export type StyledCardProps = {
     cardSize: string,
     radiusSize: string,
     backgroundColor: string
 }
 
-const StyledCard = styled.div<StyledCardProps>(props => `
+export const StyledCard = styled.div<StyledCardProps>(props => `
     display: inline-block;
     width:${props.cardSize};
     height:${props.cardSize};
     background:${props.backgroundColor};
 
     border-radius: ${props.radiusSize};
-    box-shadow: 0px 7px 8px 0 rgba(0, 0, 0, 0.2);
+    box-shadow: 0px 4px 8px 0 rgba(0, 0, 0, 0.3);
 `)
