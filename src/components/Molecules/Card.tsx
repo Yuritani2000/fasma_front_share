dc2bf8c19c63a92347d620175820fc8a80cac594
Takashi Nishikawa <program.takashi@gmail.com>
2020-11-16 17:29:29 +0900

    [fix] カードのサイズを固定の物と自由記述の両方指定できるように変更

--- src/components/Molecules/Card.tsx
+++ src/components/Molecules/Card.tsx
@@ -2,28 +2,57 @@ import React from 'react'
 import styled from 'styled-components';
 
 type Props = {
-    childComponent?: React.ReactNode[],
+    childComponent?: React.ReactNode,
     cardProps: StyledCardProps,
 }
 
 export type StyledCardProps = {
-    cardHeightSize?: string,
-    cardWidthSize?: string,
-    radiusSize: string,
+    styledCardSize: StyledCardSizeType,
     backgroundColor: string,
+}
+
+export type StyledCardSizeType = {
+    cardHeightSize: number,
+    cardWidthSize: number,
+    radiusSize: number
+}
+
+export const StyledCardSizeDefault = {
+    // #1~3 に対応
+    TOP: {
+        "cardHeightSize": 418,
+        "cardWidthSize": 319,
+        "radiusSize": 5
+    },
+    // #4 に対応
+    SKILL: {
+        "cardHeightSize": 238,
+        "cardWidthSize": 177,
+        "radiusSize": 5
+    },
+    // #6 に対応
+    LISTING: {
+        "cardHeightSize": 382,
+        "cardWidthSize": 319,
+        "radiusSize": 5
+    },
+    // #9 に対応
+    PURCHAISE: {
+        "cardHeightSize": 376,
+        "cardWidthSize": 319,
+        "radiusSize": 5
+    }
 }
 
 const Card: React.FC<Props> = props => {
     const {
-        cardHeightSize,
-        cardWidthSize,
-        radiusSize,
+        styledCardSize,
         backgroundColor
     } = props.cardProps
     return (
-        <StyledCard cardHeightSize={cardHeightSize} cardWidthSize={cardWidthSize} radiusSize={radiusSize} backgroundColor={backgroundColor}>
+        <StyledCard styledCardSize={styledCardSize} backgroundColor={backgroundColor}>
             <StyledCenteringDiv>
-                {props.childComponent ? props.childComponent.map((component) => { return component }) : <div />}
+                {props.childComponent ? props.childComponent : <div />}
             </StyledCenteringDiv>
         </StyledCard >
     )
@@ -36,9 +65,9 @@ export const StyledCard = styled.div<StyledCardProps>(props => `
     background:${props.backgroundColor};
     overflow:hidden;
 
-    width:${props.cardWidthSize ? props.cardWidthSize : "auto"};
-    height:${props.cardHeightSize ? props.cardHeightSize : "auto"};    
-    border-radius: ${props.radiusSize};
+    width:${props.styledCardSize.cardWidthSize + "px" ? props.styledCardSize.cardWidthSize + "px" : "auto"};
+    height:${props.styledCardSize.cardHeightSize + "px" ? props.styledCardSize.cardHeightSize + "px" : "auto"};    
+    border-radius: ${props.styledCardSize.radiusSize + "px"};
 
     box-shadow: 0px 3px 8px 0 rgba(0, 0, 0, 0.25);
 `)
