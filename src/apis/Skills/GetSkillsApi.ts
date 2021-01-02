7200e43aeca8a7bb7e0e2755340ad9b33a1471cd
ShotaroOkada <b1016117@gmail.com>
2021-01-02 20:38:02 +0900

    [refact]api argument

---
+++ src/apis/Skills/GetSkillsApi.ts
@@ -0,0 +1,28 @@
+import Axios from "../Axios";
+import SkillModel from "../Models/SkillModel";
+
+export type GetSkillsParam = {
+  category: string
+  number: number
+  quantity: number
+  sort: string
+  token: string
+}
+
+export async function getSkillsApi(params: GetSkillsParam) {
+  try {
+    return await Axios.get<SkillModel[]>('/skills', {
+      headers: {
+        authorization: params.token
+      },
+      params: {
+        category: params.category,
+        number: params.number,
+        quantity: params.quantity,
+        sort: params.sort
+      }
+    })
+  } catch (e) {
+    throw new Error(e)
+  }
+};
