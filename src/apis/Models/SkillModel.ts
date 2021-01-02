cdbe53169ae94c40ee922b52f8c6437fad9ebc96
ShotaroOkada <b1016117@gmail.com>
2021-01-02 20:36:36 +0900

    [refact]cut out model folder

---
+++ src/apis/Models/SkillModel.ts
@@ -0,0 +1,11 @@
+type SkillModel = {
+  id: number,
+  name: string,
+  thumbnail: string,
+  description: string,
+  price: number,
+  category: string,
+  tags: string[]
+}
+
+export default SkillModel
---
+++ src/apis/Models/UserModel.ts
@@ -0,0 +1,14 @@
+type UserModel = {
+  id: number,
+  name: string,
+  avatar: string,
+  introduction: string,
+  google_emial: string,
+  school_email: string,
+  other_email: string,
+  qr_code: string,
+  created_at: Date,
+  updated_at: Date
+}
+
+export default UserModel;
