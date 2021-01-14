aeb7d6b408066a11bb100bb895959ec550daba44
ShotaroOkada <b1016117@gmail.com>
2021-01-14 22:08:53 +0900

    [feature]top page

---
+++ src/components/Templates/TopTemplate/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from 'styled-components'
+import { TopPageSkillsState } from '../../../states/Skills'
+import Space from '../../../styles/Space'
+import Header from '../../Organisms/Header'
+import NewSkillList from '../../Organisms/NewSkillList'
+
+export type HomeTempleteProps = {
+  topPageSkills: TopPageSkillsState
+}
+
+const TopTemplate: React.FC<HomeTempleteProps> = (props) => {
+  const { topPageSkills } = props;
+  return (
+    <div>
+      <Header isSerchBox={true} />
+      <StyledNewSkillListArea>
+        {Object.entries(topPageSkills).map(([category, skills]) => {
+          return (
+            <NewSkillList key={category} category={category} skills={skills} />
+          )
+        })
+        }
+      </StyledNewSkillListArea>
+    </div>
+  )
+}
+
+export default TopTemplate;
+
+const StyledNewSkillListArea = styled.div`
+  margin-top: 8%;
+  margin-left: ${Space.BASE};
+`
