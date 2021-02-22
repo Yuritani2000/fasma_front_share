fdaef9314359f3bc21406defdd1bffd815aa53aa
b1019116 <b1019116@fun.ac.jp>
2021-02-23 01:49:29 +0900

    サンプル画像のURL差し替え

--- src/components/Molecules/SkillCardOnProfilePage/index.stories.tsx
+++ src/components/Molecules/SkillCardOnProfilePage/index.stories.tsx
@@ -37,5 +37,5 @@ export const _SearchBox = Template.bind({});
 _SearchBox.args = {
     skillName: '上手に左バック駐車をする方法aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
     skillExplanation: '免許を取ったばかりで、運転に全然なれていないという方向けです。教習所で縦列駐車と方向転換はしたと言っても、いざ駐車場で実践してみるとなると全然間隔がつかめない！バックに時間がかかりすぎて後ろの車を待たせるのが怖い！という経験はありませんか？実は左バックはコツをつかめばとっても簡単なんです！スキルをご購入いただいた方には、私の車を貸し出します（保険には事前に加入してもらいます。）。',
-    imageUrl: 'https://www.fun.ac.jp/wp-content/themes/fun_2012/img/title-ja.svg',
+    imageUrl: 'https://lh3.googleusercontent.com/pw/ACtC-3cUs8S0_2sejxXaRDAwgwz5CWlDxDHqjRp4F9z2de97GcfT9rUfhFETcW8HdtQ868dTNRUiNAcST1bTtJFPZPSYf5HEmPYlz6hD9RtNZ9t1h5vaSuuy9_oO4WYX3YGKjk9WxmakRwHzS1zN_jI51ZNK=w1736-h976-no?authuser=0',
 };
