4791b0a5c040a5c2c697ee4b388bf6efd61ee23d
ShotaroOkada <b1016117@gmail.com>
2021-04-28 15:28:25 +0900

    [add]heroku mock server port

---
+++ src/@types/node.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="node" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly REACT_APP_LOCAL_MOCK_SERVER: string;
+    readonly REACT_APP_HEROKU_MOCK_SERVER: string;
+  }
+}
--- src/apis/Axios.ts
+++ src/apis/Axios.ts
@@ -2,5 +2,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://127.0.0.1:3001/'
+  baseURL: process.env.REACT_APP_HEROKU_MOCK_SERVER
 })
