cac4e1d009f3b4a8416a06caf29fc025d359ab8f
ShotaroOkada <b1016117@gmail.com>
2021-08-01 16:34:28 +0900

    [feature]NotificationHeadingElement

---
+++ src/components/Molecules/NotificationHeadingElement/index.stories.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { Story, Meta } from "@storybook/react/types-6-0";
+import NotificationHeadingElement, { NotificationHeadingElementProps } from '.';
+
+export default {
+  title: "Molecules/NotificationHeadingElement",
+  component: NotificationHeadingElement,
+  decorators: [
+    (Story) => (<div style={{ width: '80%' }}><Story /></div>),
+  ],
+  argTypes: {
+    skillName: {
+    },
+    purchaser: {
+    },
+  },
+} as Meta;
+
+const Template: Story<NotificationHeadingElementProps> = (args) => {
+  return <NotificationHeadingElement {...args} />
+}
+
+export const _NotificationHeadingElement = Template.bind({});
+_NotificationHeadingElement.args = {
+  skillName: "アルゴの課題のお手伝い",
+  purchaser: "タコ"
+};
