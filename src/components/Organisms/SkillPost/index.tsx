e12fec5165da2c02dff3f2824c43709384fef675
b1019116 <softshiro2009@yahoo.co.jp>
2021-08-06 07:07:09 +0900

    fix pushed url after skill posting

--- src/components/Organisms/SkillPost/index.tsx
+++ src/components/Organisms/SkillPost/index.tsx
@@ -51,7 +51,7 @@ const SkillPost: React.FC<SkillPostProps> = (props) => {
             setWarning(true);
             return;
         }
-        history.push('/purchaseCompleted');
+        history.push('/post/completed');
     }
 
     const onClickSaveDraftButton = () => {
