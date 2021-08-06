40f6a815421d90b62b9ac4357eae4cede13fbceb
ShotaroOkada <b1016117@gmail.com>
2021-08-06 11:07:22 +0900

    [featue]skill post completed template

---
+++ src/components/Templates/SkillPostCompletedTemplate/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PostCompletedModal from '../../Organisms/PostCompletedModal'
+
+const SkillPostCompletedTemplate: React.FC = () => {
+  return (
+    <PostCompletedModal />
+  )
+}
+
+export default SkillPostCompletedTemplate
