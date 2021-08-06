4185aeee1d838ca0634636f07466d41836a7f7f3
ShotaroOkada <b1016117@gmail.com>
2021-08-06 13:40:38 +0900

    [add]floationg button

--- src/components/Templates/TopTemplate/index.tsx
+++ src/components/Templates/TopTemplate/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import styled from 'styled-components'
 import { TopPageSkillsState } from '../../../states/Skills'
 import Space from '../../../styles/Space'
+import FloatingActionButton from '../../Atoms/FloatingActionButton'
 import Header from '../../Organisms/Header'
 import NewSkillList from '../../Organisms/NewSkillList'
 
@@ -11,6 +13,12 @@ export type HomeTemplateProps = {
 
 const TopTemplate: React.FC<HomeTemplateProps> = (props) => {
   const { topPageSkills } = props;
+  const history = useHistory();
+
+  const handleClickPostButtom = () => {
+    history.push('/post')
+  }
+
   return (
     <div>
       <Header isSearchBox={true} />
@@ -22,6 +30,7 @@ const TopTemplate: React.FC<HomeTemplateProps> = (props) => {
         })
         }
       </StyledNewSkillListArea>
+      <FloatingActionButton label="出品" handleClick={handleClickPostButtom} />
     </div>
   )
 }
